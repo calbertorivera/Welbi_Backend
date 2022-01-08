d1f73d43bf3b6c3e39057293695959e6b27c530c
Carlos Alberto Rivera Munera <calbertorivera@hotmail.com>
2022-01-08 15:27:52 -0500

    Color changes

--- SipMind/SipMind/SipMind.Android/MainActivity.cs
+++ SipMind/SipMind/SipMind.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace SipMind.Droid
 {
-    [Activity(Label = "SipMind", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SipMind", Icon = "@mipmap/SlipMindAppIcon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
