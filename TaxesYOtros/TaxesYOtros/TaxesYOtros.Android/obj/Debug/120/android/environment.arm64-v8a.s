09f40b0bcd05a8c27ab220c15dcdc5bbfe395614
Carlos Alberto Rivera Munera <calbertorivera@hotmail.com>
2022-05-14 11:44:42 -0500

    Last Push

--- TaxesYOtros/TaxesYOtros/TaxesYOtros.Android/obj/Debug/120/android/environment.arm64-v8a.s
+++ TaxesYOtros/TaxesYOtros/TaxesYOtros.Android/obj/Debug/120/android/environment.arm64-v8a.s
@@ -85,7 +85,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"4406ea59-1e42-4cf7-ada1-890c47de0bd9"
+	.asciz	"a5b159d4-7109-4583-862c-3cb51b843175"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
